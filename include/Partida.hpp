cbd16888c82ebd0b1d7ba6f6928f14a3915ddf81
Natalia Goncalves <97234051+NataliaGoncalves08@users.noreply.github.com>
2024-07-22 16:49:19 -0300

    Update Partida.hpp
    Acompanha as mudanças do cpp

--- include/Partida.hpp
+++ include/Partida.hpp
@@ -1,1 +1,19 @@
- 
+#ifndef PARTIDA_HPP
+#define PARTIDA_HPP
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Partida {
+  public:
+    Partida(string t_jogo, string apl1, string apl2) : _t_jogo(t_jogo), _apl1(apl1), _apl2(apl2) {}
+    void iniciar_jogo();
+    string get_apl1() const;
+    string get_apl2() const;
+
+  private:
+    string _t_jogo, _apl1, _apl2;
+};
+
+#endif
