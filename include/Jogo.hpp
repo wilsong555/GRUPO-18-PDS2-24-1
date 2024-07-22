4d76c1d7458b6a303558bddbe11f4c88f7d39e92
Natalia Goncalves <97234051+NataliaGoncalves08@users.noreply.github.com>
2024-07-22 16:48:17 -0300

    Update Jogo.hpp
    Acompanha mudanças do cpp

--- include/Jogo.hpp
+++ include/Jogo.hpp
@@ -5,8 +5,6 @@
 #include <vector>
 #include <string>
 
-
-
 using namespace std;
 
 class Jogo {
@@ -15,10 +13,12 @@ class Jogo {
     vector <vector <string>> matriz;
 
     public:
-    //Jogo();
     Jogo(int linha, int coluna);
-    string getvalor_matrz(int linha, int coluna) const;
+    string getvalor_matriz(int linha, int coluna) const;
+    int get_linhas() const;
+    int get_colunas() const;
     void setvalor(int linha, int coluna, string valor);
+    virtual bool verificar_vencedor() = 0;
     void imprime();
 
 };
