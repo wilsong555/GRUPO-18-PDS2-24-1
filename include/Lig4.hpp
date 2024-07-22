5fd4918e24e034485a215dc301742ea210cdacd6
Natalia Goncalves <97234051+NataliaGoncalves08@users.noreply.github.com>
2024-07-22 16:48:48 -0300

    Update Lig4.hpp
    Acompanha mudanças do cpp

---
+++ include/Lig4.hpp
@@ -0,0 +1,17 @@
+#ifndef LIG4_HPP
+#define LIG4_HPP
+
+#include "Jogo.hpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Lig4 : public Jogo {
+  public:
+    Lig4(int num_linha, int num_coluna) : Jogo(num_linha, num_coluna) {}
+    void jogar(int coluna, string jogador);
+    bool verificar_vencedor() override;
+};
+
+#endif
