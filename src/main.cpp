3e014db72ec8c1cfe0256490312a52967b4bdf6c
Natalia Goncalves <97234051+NataliaGoncalves08@users.noreply.github.com>
2024-07-22 16:42:20 -0300

    Add Partida
    Implementei algumas partes do jogo LIG4 e da classe Partida

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,16 @@
 #include <string>
 #include <cctype>
 #include "../include/CadastroJogadores.hpp"
+#include "../include/Partida.hpp"
 #include "../include/Jogo.hpp"
 
 
 using namespace std;
 
 int main() {
-    string escolha, nome, apelido;
-     int linha, coluna;
-     string valor_tabuleiro;
-  
+    string escolha;
+    string nome, apelido;
+    Cadastro novo_cadastro;
     while (escolha != "FS") {
            cout << "Faca a escolha" << endl;
             cin >> escolha;
@@ -21,36 +21,43 @@ int main() {
                 l = toupper(l);
             }
             if (escolha == "CJ") {
-                Cadastro();
+                cout << "Digite o nome e o apelido: ";
+                cin >> nome >> apelido;
+                cout << novo_cadastro.adicionar_jogador(nome, apelido) << endl;
             }
             else if (escolha == "RJ") {
-                Cadastro::remover_jogador();
+                cout << "Digite o apelido do jogador: ";
+                cin >> apelido;
+                if(novo_cadastro.remover_jogador(apelido)) {
+                    cout << "Jogador removido com sucesso" << endl;
+                }
+                else {
+                    cout << "Jogador não encontrado" << endl;
+                }
             }
             else if (escolha == "LJ") {
                 Cadastro::listar_jogadores();
             }
+            else if (escolha == "CP") {
+                string apl1, nome1, apl2, nome2, jogo;
+                
+                cout << "Digite L para Lig4 ou R para Reversi: ";
+                cin >> jogo;
+                cout << "Digite o nome e o nick do jogador 1: " << endl;
+                cin >> apl1 >> nome1;
+                cout << "Digite o nome e o nick do jogador 2: " << endl;
+                cin >> apl2 >> nome2;
+                if (jogo == "L") {
+                    if (novo_cadastro.verificar_jogador(nome, apelido)) {
+                        Partida nova_partida(jogo, apl1, apl2);
+                        nova_partida.iniciar_jogo();
+                    }
+                }
+
+            }  
             else if (escolha == "FS") {
                 return 0;
             }
-
-                 else if (escolha == "T") {
-                    cout << "digite  quantidade de lnhas: " << endl;
-                    cin >> linha;
-                    cout << "agora digite a quntidade de colunas: ";
-                    cin >> coluna;
-
-                     Jogo obj(linha, coluna);
-                     cout<< "escolha a linha e a coluna desejada :";
-                    cin >> linha >> coluna;
-                    cout << "Digite X ou O";
-                    cin >> valor_tabuleiro;
-                    obj.setvalor(linha - 1, coluna - 1 , valor_tabuleiro);
-                    obj.imprime();
-            
-    }
-
          }
-        
-
     return 0;
 }
